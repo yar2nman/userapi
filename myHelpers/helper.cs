5814af9695032a31f1311c9600cf0207829a6c4c
Ahmed Taher <yar2nman@gmail.com>
2020-03-28 08:01:25 +0300

    commenting Patch and helper connstr

--- myHelpers/helper.cs
+++ myHelpers/helper.cs
@@ -6,8 +6,20 @@ using Microsoft.EntityFrameworkCore;
 
 namespace usersapi {
 static class helper {
+    /// <summary>
+    /// string holding the conniction string
+    /// It is populated using the secret item or the enviroment variable
+    /// Use ConnStr to hold the value of the connection string in the secrete or in the enviroment variables
+    /// </summary>
     public static string connstr = "";
 
+
+/// <summary>
+/// function to update entity using input object and save the updated to DB
+/// </summary>
+/// <param name="entity">The entity that we need to Patch</param>
+/// <param name="patchObject">Object with keys and values to be updated</param>
+/// <returns></returns>
     public static async Task ApplyPatchAsync(object entity, object patchObject)
 {
     DbContext _context = new usersapi.Models.DataContext();
