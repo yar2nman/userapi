208c6986f971198c037dcc0d655c5facf6aaae89
Ahmed Taher <yar2nman@gmail.com>
2020-04-03 15:24:06 +0300

    cors

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,7 @@ namespace usersapi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [EnableCors("MyCorsPolicy")]
     public class UsersController : ControllerBase
     {
         usersapi.Models.DataContext _context;
--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,13 @@ namespace usersapi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(o => o.AddPolicy("MyCorsPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+
             services.AddControllers();
         }
 
